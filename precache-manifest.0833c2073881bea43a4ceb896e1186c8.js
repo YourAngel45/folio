70f080b81e8fc8ff0012864b9ac7ca66c6b27b76
Alexandre Barone (from Travis CI) <abalexandrebarone@gmail.com>
2019-05-25 20:36:02 +0000

    Deploy YourAngel45/folio to github.com/YourAngel45/folio.git:gh-pages

---
+++ precache-manifest.0833c2073881bea43a4ceb896e1186c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a563d3d57c176249913908ebfd038d12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ada95764c94a730c20be",
+    "url": "/static/css/main.90820d10.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "ada95764c94a730c20be",
+    "url": "/static/js/main.bf50b1ad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a267e2e0e21068a744c2e0b6cbe02dfb",
+    "url": "/static/media/004-cpu.a267e2e0.svg"
+  }
+]);
